5ab0ccdfe2f03f95e83d17eb87113ebd97a9caaa
Emma <k54979802@gmail.com>
2016-08-19 06:13:26 +0000

    增加註解

--- testAPI/models/Bet.php
+++ testAPI/models/Bet.php
@@ -8,6 +8,11 @@ require_once 'Database.php';
 
 class Bet extends Database
 {
+    /**
+     * 取得資料庫中所的資料其id與更新時間
+     *
+     * @return array
+     */
     public function selectAll() {
         $sql = "SELECT `bID`, `updateTime` FROM `betting`";
         $result = $this->prepare($sql);
@@ -18,6 +23,12 @@ class Bet extends Database
         return $betData;
     }
 
+    /**
+     * 取得資料庫中編號$id的資料
+     *
+     * @param string $id 資料id
+     * @return array
+     */
     public function selectContent($id) {
         $sql = "SELECT * FROM `betting` WHERE `bID` = :id";
         $result = $this->prepare($sql);
